476806c3d7940c37dcf324b4d113d09bd37fb978
liyang <599283041@qq.com>
2021-01-27 14:24:20 +0800

    提交Glide的二次封装

---
+++ aggregate-methods/src/main/java/com/example/aggregate_methods/tools/glide/GlideEngine.java
@@ -0,0 +1,87 @@
+package com.example.aggregate_methods.tools.glide;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.request.RequestOptions;
+
+/**
+ * CREATE BY liyang
+ * ON 2021-01-27
+ * SUPPLY : Thanks for watching
+ */
+public class GlideEngine implements ImageEngine {
+
+    private static GlideEngine engine;
+
+    public static GlideEngine instantiation() {
+        if (null == engine) {
+            synchronized (GlideEngine.class) {
+                if (null == engine)
+                    engine = new GlideEngine();
+            }
+        }
+        return engine;
+    }
+
+    @Override
+    public void loadImage(Context context, ImageView imageView, Object path) {
+        Glide.with(context)
+                .load(path)
+                .apply(new RequestOptions()
+                        .priority(Priority.HIGH)
+                        .centerCrop())
+                .into(imageView);
+    }
+
+    @Override
+    public void loadImage(Context context, int resizeX, int resizeY, ImageView imageView, Uri uri) {
+        Glide.with(context)
+                .load(uri)
+                .apply(new RequestOptions()
+                        .override(resizeX, resizeY)
+                        .priority(Priority.HIGH)
+                        .centerCrop())
+                .into(imageView);
+    }
+
+    @Override
+    public void loadGifImage(Context context, int resizeX, int resizeY, ImageView imageView, Uri uri) {
+        Glide.with(context)
+                .asGif()
+                .load(uri)
+                .apply(new RequestOptions()
+                        .override(resizeX, resizeY)
+                        .priority(Priority.HIGH)
+                        .fitCenter())
+                .into(imageView);
+    }
+
+    @Override
+    public void loadThumbnail(Context context, int resize, Drawable placeholder, ImageView imageView, Uri uri) {
+        Glide.with(context)
+                .asBitmap() // some .jpeg files are actually gif
+                .load(uri)
+                .apply(new RequestOptions()
+                        .override(resize, resize)
+                        .placeholder(placeholder)
+                        .centerCrop())
+                .into(imageView);
+    }
+
+    @Override
+    public void loadGifThumbnail(Context context, int resize, Drawable placeholder, ImageView imageView, Uri uri) {
+        Glide.with(context)
+                .asBitmap() // some .jpeg files are actually gif
+                .load(uri)
+                .apply(new RequestOptions()
+                        .override(resize, resize)
+                        .placeholder(placeholder)
+                        .centerCrop())
+                .into(imageView);
+    }
+}
---
+++ aggregate-methods/src/main/java/com/example/aggregate_methods/tools/glide/ImageEngine.java
@@ -0,0 +1,66 @@
+package com.example.aggregate_methods.tools.glide;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.widget.ImageView;
+
+/**
+ * CREATE BY liyang
+ * ON 2021-01-27
+ * SUPPLY : Thanks for watching
+ */
+public interface ImageEngine {
+    /**
+     * 加载网络图片
+     *
+     * @param context
+     * @param imageView
+     * @param path
+     */
+    void loadImage(Context context, ImageView imageView, Object path);
+
+    /**
+     * 加载网络图片
+     *
+     * @param context
+     * @param resizeX
+     * @param resizeY
+     * @param imageView
+     * @param uri
+     */
+    void loadImage(Context context, int resizeX, int resizeY, ImageView imageView, Uri uri);
+
+    /**
+     * 加载动态GIF
+     *
+     * @param context
+     * @param resizeX
+     * @param resizeY
+     * @param imageView
+     * @param uri
+     */
+    void loadGifImage(Context context, int resizeX, int resizeY, ImageView imageView, Uri uri);
+
+    /**
+     * 加载缩略图
+     *
+     * @param context
+     * @param resize
+     * @param placeholder
+     * @param imageView
+     * @param uri
+     */
+    void loadThumbnail(Context context, int resize, Drawable placeholder, ImageView imageView, Uri uri);
+
+    /**
+     * 加载缩略GIF
+     *
+     * @param context
+     * @param resize
+     * @param placeholder
+     * @param imageView
+     * @param uri
+     */
+    void loadGifThumbnail(Context context, int resize, Drawable placeholder, ImageView imageView, Uri uri);
+}
