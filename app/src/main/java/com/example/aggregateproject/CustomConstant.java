60e7095070594ad79392a35973bb1cc6e2dad989
liyang <599283041@qq.com>
2021-03-09 15:30:44 +0800

    添加移动imageView

--- app/src/main/java/com/example/aggregateproject/CustomConstant.java
+++ app/src/main/java/com/example/aggregateproject/CustomConstant.java
@@ -10,6 +10,6 @@ public class CustomConstant {
     //    public static String imagePath = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa0.att.hudong.com%2F30%2F29%2F01300000201438121627296084016.jpg&refer=http%3A%2F%2Fa0.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616745458&t=7d82e31e52056875a4c05f06fe76b63e";
     public static String imagePath = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg1001.pocoimg.cn%2Fimage%2Fpoco%2Fworks%2F79%2F2012%2F0322%2F20%2F62853829201203222023101923154827283_004_62853829.jpg&refer=http%3A%2F%2Fimg1001.pocoimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616823062&t=acad629e36b63f73de84a4eed4381311";
 
-    public static String[] conductList = { "有人能火，为什么不能是我", "我不是药神", "我，不是说了能力要平均值么！", "我不是白痴",
+    public static String[] conductList = {"有人能火，为什么不能是我", "我不是药神", "我，不是说了能力要平均值么！", "我不是白痴",
             "我不是张医城", "我不是腿毛酱", "我不是凶手", "我不是叫教主", "我不是教主 动态漫", "我不是说了能力要平均值嗎！(僅限港澳台地區)"};
 }
