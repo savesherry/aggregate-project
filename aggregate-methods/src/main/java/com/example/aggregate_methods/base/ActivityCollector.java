a5cc27259b216a2b3c27c20e3262d48b417a20e4
lyang <599283041@qq.com>
2022-01-02 17:16:19 +0800

    添加删除控制器

--- aggregate-methods/src/main/java/com/example/aggregate_methods/base/ActivityCollector.java
+++ aggregate-methods/src/main/java/com/example/aggregate_methods/base/ActivityCollector.java
@@ -21,6 +21,31 @@ public class ActivityCollector {
         list.remove(activity);
     }
 
+    /**
+     * 除了传来的Activity其他的全部删除
+     * 可以传多个Activity
+     *
+     * @param cla
+     */
+    public static void removeAll(Class<?>... cla) {
+        boolean isExist = false;
+        for (Activity activity : list) {
+            for (Class c : cla) {
+                if (activity.getClass().isAssignableFrom(c)) {
+                    isExist = true;
+                    break;
+                }
+            }
+            if (!isExist) {
+                if (!activity.isFinishing()) {
+                    activity.finish();
+                }
+            } else {
+                isExist = false;
+            }
+        }
+    }
+
     public static void finishAll() {
         for (Activity activity : list) {
             if (!activity.isFinishing()) {
