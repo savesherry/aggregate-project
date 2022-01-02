0c87b3c99fe8f7dbc1b4bd4d5c051e738ec22b08
lyang <599283041@qq.com>
2022-01-02 14:07:01 +0800

    提交

--- aggregate-methods/src/main/java/com/example/aggregate_methods/tools/permissions/ConstantPermission.java
+++ aggregate-methods/src/main/java/com/example/aggregate_methods/tools/permissions/ConstantPermission.java
@@ -3,7 +3,7 @@ package com.example.aggregate_methods.tools.permissions;
 /**
  * CREATE BY LiYang
  * ON 2021-02-18
- * SUPPLY : Thanks for watching
+ * SUPPLY :  Thanks for watching
  */
 public class ConstantPermission {
 
